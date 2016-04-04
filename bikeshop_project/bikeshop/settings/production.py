5a2e802ed91e323178ab87a52d0702bcc498692e
Drew Larson <drew@drwlrsn.com>
2016-04-03 19:42:02 -0600

    Production settings.

---
+++ bikeshop_project/bikeshop/settings/production.py
@@ -0,0 +1,44 @@
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+WSGI_APPLICATION = "bikeshop.wsgi.application"
+
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["shop.bcbc.bike"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bikeshop_production",
+        "USER": "bikeshop",
+        "PASSWORD": os.environ["DJANGO_DB_PASSWORD"],
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "verbose"},
+    },
+    "formatters": {
+        "verbose": {"format": "%(levelname)s %(asctime)s %(pathname)s %(message)s"},
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "ERROR"),
+        },
+        "bikeshop": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+        },
+    },
+}

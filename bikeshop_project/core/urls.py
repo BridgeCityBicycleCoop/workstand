3296ca3b9f9c59320ef5fc7a957e78b7ab4fc84c
Drew Larson <drew@drwlrsn.com>
2016-05-25 17:59:54 -0600

    Some stuff I did. Sorry Arlin.

--- bikeshop_project/core/urls.py
+++ bikeshop_project/core/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import DashboardView
+from .views import DashboardView, NewMembershipView
 
 urlpatterns = [
+    url(r"^membership/new/$", NewMembershipView.as_view()),
     url(r"^$", DashboardView.as_view()),
 ]

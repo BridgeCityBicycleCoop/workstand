60d4bacdfb7dcccce517a8cc14f2297440f66090
Drew Larson <drew@drwlrsn.com>
2016-05-25 18:46:33 -0600

    Add submit form.

--- bikeshop_project/core/urls.py
+++ bikeshop_project/core/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import DashboardView, NewMembershipView
 
 urlpatterns = [
-    url(r"^membership/new/$", NewMembershipView.as_view()),
+    url(r"^membership/new/$", NewMembershipView.as_view(), name="new_membership"),
     url(r"^$", DashboardView.as_view()),
 ]

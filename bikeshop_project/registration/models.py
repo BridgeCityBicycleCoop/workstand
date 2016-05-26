4a01e29b9fcaf5b96ce33671ed75e5fe1443bf8e
Drew Larson <drew@drwlrsn.com>
2016-05-26 10:26:23 -0600

    Move membership to Member.

--- bikeshop_project/registration/models.py
+++ bikeshop_project/registration/models.py
@@ -59,6 +59,13 @@ class Member(AbstractBaseUser, PermissionsMixin):
     waiver = models.DateTimeField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    membership = models.OneToOneField(
+        'core.Membership',
+        on_delete=models.CASCADE,
+        related_name='member',
+        blank=True,
+        null=True
+    )
 
     objects = CustomMemberManager()
 

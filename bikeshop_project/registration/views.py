f06307632941d8f0ecac29164f21744de6fce8a4
Drew Larson <drew@drwlrsn.com>
2016-05-26 10:27:25 -0600

    Edit member WIP.

--- bikeshop_project/registration/views.py
+++ bikeshop_project/registration/views.py
@@ -1,14 +1,27 @@
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from django.utils import timezone
 from django.views.generic import View
 
 from .forms import MemberForm
+from .models import Member
+import logging
+
+logger = logging.getLogger(__file__)
 
 
 class MemberFormView(View):
-    def get(self, request):
-        form = MemberForm()
-        context = {"form": form}
+    def get(self, request, member_id=None):
+        try:
+            logger.debug(member_id)
+            member = Member.objects.get(id=member_id)
+            form = MemberForm(instance=member)
+        except Member.DoesNotExist:
+            form = MemberForm()
+
+        context = dict(form=form)
+        if form.instance:
+            return TemplateResponse(request, "edit_member_form.html", context=context)
         return TemplateResponse(request, "member_form.html", context=context)
 
     def post(self, request):

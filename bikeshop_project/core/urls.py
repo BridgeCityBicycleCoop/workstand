4181fd951fdc5039345326cba09fdffd4e66e8fb
Drew Larson <drew@drwlrsn.com>
2016-05-30 20:31:53 -0600

    New membership url.

--- bikeshop_project/core/urls.py
+++ bikeshop_project/core/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 
 from .views import DashboardView, NewMembershipView
 urlpatterns = [
-    url(r'^membership/new/$', NewMembershipView.as_view(), name='new_membership'),
+    url(r'^member/(?P<member_id>[0-9]+)/membership/new/$', NewMembershipView.as_view(), name='new_membership'),
     url(r'^$', DashboardView.as_view())
 ]

c906b710ba4402fb5bb24ae63fd55a4f1a18c404
Drew Larson <drew@drwlrsn.com>
2016-06-23 11:53:01 -0600

    Fix login/logout urls.

--- bikeshop_project/bikeshop/settings/base.py
+++ bikeshop_project/bikeshop/settings/base.py
@@ -151,3 +151,6 @@ HAYSTACK_CONNECTIONS = {
         'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
     },
 }
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- bikeshop_project/core/urls.py
+++ bikeshop_project/core/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import DashboardView, NewMembershipView
 urlpatterns = [
     url(r'^member/(?P<member_id>[0-9]+)/membership/new/$', NewMembershipView.as_view(), name='new_membership'),
-    url(r'^$', DashboardView.as_view())
+    url(r'^$', DashboardView.as_view(), name='home')
 ]

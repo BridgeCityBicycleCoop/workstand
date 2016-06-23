f6a557114c44f251ce04aab4c7eab5401af4b9b5
Drew Larson <drew@drwlrsn.com>
2016-06-23 14:39:17 -0600

    Add members list.

--- bikeshop_project/registration/urls.py
+++ bikeshop_project/registration/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import MemberFormView, MemberSearchView, MemberSignIn
+from .views import MemberFormView, MemberSearchView, MemberSignIn, Members
 
 urlpatterns = [
     url(r"^new/$", MemberFormView.as_view(), name="member_new"),
@@ -11,4 +11,5 @@ urlpatterns = [
     ),
     url(r"^edit/(?P<member_id>[0-9]+)/$", MemberFormView.as_view(), name="member_edit"),
     url(r"^signin/$", MemberSignIn.as_view(), name="member_signin"),
+    url(r"^$", Members.as_view(), name="members"),
 ]

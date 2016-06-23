b614a2b1a6541a85980b3196dfc15b9df7fc83a0
Drew Larson <drew@drwlrsn.com>
2016-06-22 19:37:59 -0600

    Signin api.

--- bikeshop_project/registration/urls.py
+++ bikeshop_project/registration/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import MemberFormView, MemberSearchView
+from .views import MemberFormView, MemberSearchView, MemberSignIn
 urlpatterns = [
     url(r'^new/$', MemberFormView.as_view(), name='signup'),
     url(r'^search/(?P<query>[\w@\.\+]+)/$', MemberSearchView.as_view(), name='member_search'),
-    url(r'^edit/(?P<member_id>[0-9]+)/$', MemberFormView.as_view(), name='member_edit')
+    url(r'^edit/(?P<member_id>[0-9]+)/$', MemberFormView.as_view(), name='member_edit'),
+    url(r'^signin/$', MemberSignIn.as_view(), name='member_signin')
 ]

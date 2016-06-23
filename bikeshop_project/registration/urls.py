3f44e259447536d5e87073c0ffe4c9d0840972de
Drew Larson <drew@drwlrsn.com>
2016-06-23 13:52:34 -0600

    Better name.

--- bikeshop_project/registration/urls.py
+++ bikeshop_project/registration/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from .views import MemberFormView, MemberSearchView, MemberSignIn
 urlpatterns = [
-    url(r'^new/$', MemberFormView.as_view(), name='signup'),
+    url(r'^new/$', MemberFormView.as_view(), name='member_new'),
     url(r'^search/(?P<query>[\w@\.\+]+)/$', MemberSearchView.as_view(), name='member_search'),
     url(r'^edit/(?P<member_id>[0-9]+)/$', MemberFormView.as_view(), name='member_edit'),
     url(r'^signin/$', MemberSignIn.as_view(), name='member_signin')

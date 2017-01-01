b65bff93992ba7e0f3448c32ef826407b258f27e
Drew Larson <drew@drwlrsn.com>
2017-01-01 17:31:57 -0600

    Add Visit to admin.

--- bikeshop_project/core/admin.py
+++ bikeshop_project/core/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Membership, Payment, Visit
 
 # Register your models here.
-admin.site.register([Membership, Payment, Visit])
+admin.site.register([Membership, Payment])
+
+
+@admin.register(Visit)
+class VistAdmin(admin.ModelAdmin):
+    ordering = ("created_at",)
+    list_display = ("member", "purpose", "created_at")

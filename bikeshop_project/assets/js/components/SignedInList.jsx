14266c57e6ebe9e18070c3e452ae17f394b03259
Drew Larson <drew@drwlrsn.com>
2017-01-01 21:01:28 -0600

    Linting.

--- bikeshop_project/assets/js/components/SignedInList.jsx
+++ bikeshop_project/assets/js/components/SignedInList.jsx
@@ -1,41 +1,37 @@
+import { ListItem } from 'material-ui/List';
 import React from 'react';
-import {List, ListItem} from 'material-ui/List';
 
 export default class SignedInList extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {tick: 0};
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.componentWillUnmount = this.componentWillUnmount.bind(this);
-        this.tick = this.tick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { tick: 0 };
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentWillUnmount = this.componentWillUnmount.bind(this);
+    this.tick = this.tick.bind(this);
+  }
 
-    componentDidMount () {
-        this.timer = setInterval(this.tick, 50);
-    }
+  componentDidMount() {
+    this.timer = setInterval(this.tick, 50);
+  }
 
-    componentWillUnmount () {
-        clearInterval(this.timer);
-    }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
 
-    tick () {
-        this.setState({tick: this.state.tick++});
-    }
+  tick() {
+    this.setState({ tick: this.state.tick += 1 });
+  }
 
-    render () {
-        const members = this.props.members.sort((l, r) => {
-                return l.at.diff(r.at);
-            })
+  render() {
+    const members = this.props.members.sort((l, r) => l.at.diff(r.at))
             .reverse()
-            .map((member) => {
-                return <ListItem key={member.id} primaryText={member.text} secondaryText={`${member.purpose} – ${member.at.fromNow()}`} />
-        });
-        
-        return (
-            <div className="mdl-cell mdl-cell--12-col">
-                <h3>Members signed in</h3>
-                {!!members.length ? members : 'No members currently signed in.'}
-            </div>
-        );
-    }
+            .map(member => <ListItem key={member.id} primaryText={member.text} secondaryText={`${member.purpose} – ${member.at.fromNow()}`} />);
+
+    return (
+      <div className="mdl-cell mdl-cell--12-col">
+        <h3>Members signed in</h3>
+        {members.length ? members : 'No members currently signed in.'}
+      </div>
+    );
+  }
 }

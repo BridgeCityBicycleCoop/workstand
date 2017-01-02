25ee85064a8526bf5de7b829c54d0b273e2760ab
Drew Larson <drew@drwlrsn.com>
2017-01-01 21:09:01 -0600

    Linting.

--- bikeshop_project/assets/js/components/Member.jsx
+++ bikeshop_project/assets/js/components/Member.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
+import React from 'react';
 
 export default class Member extends React.Component {
-    render () {
-        return (
-            <AutoComplete
-                dataSource={this.props.members}
-                onUpdateInput={this.props.handleUpdate.bind(this)}
-                openOnFocus={true}
-                filter={AutoComplete.noFilter}
-                onNewRequest={this.props.signIn.bind(this)}
-                errorText={this.props.error}
-                hintText="Search members"
-                searchText={this.props.searchText}
-                fullWidth={true}
-                textFieldStyle={{textAlign: 'center', fontSize: '32px', lineHeight: '48px'}}
-                tabIndex={this.props.tabIndex}
-            />
-        );
-    }
+  render() {
+    return (
+      <AutoComplete
+        dataSource={this.props.members}
+        onUpdateInput={this.props.handleUpdate.bind(this)}
+        openOnFocus
+        filter={AutoComplete.noFilter}
+        onNewRequest={this.props.signIn.bind(this)}
+        errorText={this.props.error}
+        hintText="Search members"
+        searchText={this.props.searchText}
+        fullWidth
+        textFieldStyle={{ textAlign: 'center', fontSize: '32px', lineHeight: '48px' }}
+        tabIndex={this.props.tabIndex}
+      />
+    );
+  }
 }

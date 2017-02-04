1da39251151f9ff096de72709467ec6f31c80288
Drew Larson <drew@drwlrsn.com>
2017-02-04 08:57:50 -0600

    Add id field.

--- bikeshop_project/registration/serializers.py
+++ bikeshop_project/registration/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
 
 from .models import Member
 
@@ -7,6 +6,7 @@ from .models import Member
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Member
-        fields = ('email', 'email_consent', 'email_consent', 'first_name', 'last_name', 'preferred_name',
+        fields = ('id', 'email', 'email_consent', 'email_consent', 'first_name', 'last_name', 'preferred_name',
                   'date_of_birth', 'guardian_name', 'phone', 'street', 'city', 'province', 'country', 'post_code',
                   'waiver', 'is_active', 'banned', 'suspended')
+        id = serializers.ReadOnlyField()

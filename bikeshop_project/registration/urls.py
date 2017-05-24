00877e22b32b4ed7264067f0479bc3e9b9826f6a
Drew Larson <drew@drwlrsn.com>
2017-05-23 18:29:10 -0600

    Fix: Search path supports queries with all non-whitespace characters.

--- bikeshop_project/registration/urls.py
+++ bikeshop_project/registration/urls.py
@@ -8,7 +8,7 @@ apiRoutes = (
 
 urlpatterns = [
     url(r'^new/$', MemberFormView.as_view(), name='member_new'),
-    url(r'^search/(?P<query>[\w@\.\+]+)/$', MemberSearchView.as_view(), name='member_search'),
+    url(r'^search/(?P<query>[\w|\W]+)/$', MemberSearchView.as_view(), name='member_search'),
     url(r'^edit/(?P<member_id>[0-9]+)/$', MemberFormView.as_view(), name='member_edit'),
     url(r'^signin/$', MemberSignIn.as_view(), name='member_signin'),
     url(r'^$', Members.as_view(), name='members'),

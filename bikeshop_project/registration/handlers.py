c316da4f28e1b6e1c7898b6250fb84f3ef119fee
Drew Larson <drew@drwlrsn.com>
2017-05-29 13:56:33 -0600

    hotfix: Spell unsubscribed correctly.

--- bikeshop_project/registration/handlers.py
+++ bikeshop_project/registration/handlers.py
@@ -18,8 +18,8 @@ def update_mailchimp(sender, instance, **kwargs):
             response = client.lists.members.create_or_update('1c664549e2',
                                                              hashlib.md5(bytes(instance.email, 'utf-8')).hexdigest(), {
                 'email_address': instance.email,
-                'status': 'subscribed' if instance.email_consent else 'unsuscribed',
-                'status_if_new': 'subscribed' if instance.email_consent else 'unsuscribed',
+                'status': 'subscribed' if instance.email_consent else 'unsubscribed',
+                'status_if_new': 'subscribed' if instance.email_consent else 'unsubscribed',
                 'merge_fields': {
                     'FNAME': instance.first_name,
                     'LNAME': instance.last_name,

64d333b61206127c15f73c36d75df6250ade1e83
Drew Larson <drew@drwlrsn.com>
2018-01-01 17:11:17 -0600

    Simplify entry points so base is source of truth.

--- bikeshop_project/webpack.dev.config.js
+++ bikeshop_project/webpack.dev.config.js
@@ -5,15 +5,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const config = require('./webpack.base.config.js');
 
 // Use webpack dev server
-config.entry = {
-  webpack: [
+config.entry.webpack = [
     'webpack-dev-server/client?http://192.168.99.100:3000',
     'webpack/hot/only-dev-server',
-  ],
-  signin: './assets/js/index',
-  members: './assets/js/members/index',
-  bikes: './assets/js/bikes/index',
-};
+];
 
 // override django's STATIC_URL for webpack bundles
 config.output.publicPath = 'http://192.168.99.100:3000/assets/bundles/';
@@ -40,8 +35,8 @@ config.module.loaders.push(
   },
   {
     test: /(\.scss|\.css)$/,
-    loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox'),
-  },
+    loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
+  }
 );
 
 module.exports = config;

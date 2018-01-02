8d9a629dfef58c4fe0318db365c50244476b2c1a
Drew Larson <drew@drwlrsn.com>
2018-01-01 18:15:16 -0600

    Add Bike to admin.

--- bikeshop_project/bike/admin.py
+++ bikeshop_project/bike/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Bike
+
+# Register your models here.
+admin.site.register([Bike])

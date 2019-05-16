666ee32c6f9a77179e40cdd37925d18aa5315307
Drew Larson <me@drewlarson.ca>
2019-05-15 18:19:23 -0600

    Remove Docker dependency

--- entrypoint-interface.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PYTHONUNBUFFERED=TRUE python
-DJANGO_SETTINGS_MODULE=bikeshop.settings.production
-cd /code
-python manage.py migrate --no-input &&
-python manage.py collectstatic --no-input &&
-python manage.py rebuild_index --noinput &&
-daphne -b 0.0.0.0 -p 8000 bikeshop.asgi:channel_layer
--- entrypoint-worker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PYTHONUNBUFFERED=TRUE python
-DJANGO_SETTINGS_MODULE=bikeshop.settings.production
-cd /code &&
-python manage.py runworker

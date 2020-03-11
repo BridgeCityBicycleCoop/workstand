0d49d1ad6a6a86bf9d5824508350bdea1969adbc
Drew Larson <me@drewlarson.ca>
2020-03-10 19:17:03 -0600

    Filter by name

--- assets/js/v2/Members/MembersTable.js
+++ assets/js/v2/Members/MembersTable.js
@@ -1,11 +1,81 @@
-import React from 'react';
-import { Table, Divider } from 'antd';
+import React, { useState } from 'react';
+import { Table, Divider, Icon, Button, Input } from 'antd';
 import { Link } from '@reach/router';
+
+const FilterDropdown = ({
+  setSelectedKeys,
+  selectedKeys,
+  confirm,
+  clearFilters,
+  onSearch,
+}) => (
+  <div style={{ padding: 8 }}>
+    <Input
+      placeholder={`Search name`}
+      value={selectedKeys[0]}
+      onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+      onPressEnter={confirm}
+      style={{ width: 188, marginBottom: 8, display: 'block' }}
+    />
+    <Button
+      type="primary"
+      onClick={confirm}
+      icon="search"
+      size="small"
+      style={{ width: 90, marginRight: 8 }}
+    >
+      Search
+    </Button>
+    <Button
+      onClick={clearFilters}
+      size="small"
+      style={{ width: 90 }}
+    >
+      Reset
+    </Button>
+  </div>
+);
+
+const getColumnSearchProps = dataIndex => ({
+  filterDropdown: props => <FilterDropdown {...props} />,
+  filterIcon: filtered => (
+    <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+  ),
+  onFilter: (value, record) =>
+    record[dataIndex]
+      .toString()
+      .toLowerCase()
+      .includes(value.toLowerCase()),
+  onFilterDropdownVisibleChange: visible => {
+    if (visible) {
+      setTimeout(() => this.searchInput.select());
+    }
+  },
+});
 
 export const MembersTable = ({ members }) => {
   const columns = [
-    { title: 'First Name', dataIndex: 'firstName', key: 'firstName' },
-    { title: 'Last Name', dataIndex: 'lastName', key: 'lastName' },
+    {
+      title: 'Name',
+      key: 'name',
+      dataIndex: 'name',
+      render: (_, record) => `${record.firstName} ${record.lastName}`,
+      filterDropdown: props => (
+        <FilterDropdown {...props} onSearch={handleSearch} />
+      ),
+      onFilter: (value, record) =>
+        record.firstName
+          .concat(record.lastName)
+          .toString()
+          .toLowerCase()
+          .includes(value.toLowerCase()),
+      filterIcon: filtered => (
+        <Icon
+          type="search"
+          style={{ color: filtered ? '#1890ff' : undefined }}
+        />
+      ),
+    },
     { title: 'Email', dataIndex: 'email', key: 'email' },
     {
       title: 'Actions',
